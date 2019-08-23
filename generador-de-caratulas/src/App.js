7af8f341fa78dc945907ac7523ece028eb0284de
Saul Rojas <saul.rojas@ucsp.edu.pe>
2019-08-22 22:00:09 -0500

    proyecto base con la librería create-react-app

---
+++ generador-de-caratulas/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      generador de ¿? empecemos!!!{" "}
+      <span role="img" aria-label="face">
+        🤩
+      </span>
+    </div>
+  );
+}
+
+export default App;
